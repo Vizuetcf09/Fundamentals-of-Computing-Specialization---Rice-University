c5c700ac8c3ff8b0da1c7834e24e9085cca57cf0
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2025-02-19 21:27:58 -0600

    created: Examples directory, midified: 3_stopwatch.py and rename the course directories

--- courses/11/Mini-projects/3_stopwatch.py
+++ courses/11/Mini-projects/3_stopwatch.py
@@ -1,36 +1,35 @@
 # Stopwatch: The Game
 
-# import simplegui
+import simplegui
 
 # define global variables
-time = 0
+time = 1
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
-def format(time):
-    pass
+def format():
+    global time
+    return str(time)
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
-    pass
+    return 
 
 def stop():
     pass
 
 def reset():
     pass
+
 # define event handler for timer with 0.1 sec interval
 def interval01():
     pass
 
 # define draw handler
-def timer_draw_handler():
-    pass
+def timer_draw_handler(canvas):
+    canvas.draw_text(format(), [60, 110], 24, "White")
 
-# create frame
-frame = simplegui.create_frame('Stopwatch: The Game', 200, 200)
-
-# register event handlers
+# button handlers
 def start_btn_handler():
     pass
 
@@ -40,10 +39,16 @@ def stop_btn_handler():
 def reset_btn_handler():
     pass
 
+# create frame
+frame = simplegui.create_frame('Stopwatch: The Game', 200, 200)
+
 # start frame
+frame.set_draw_handler(timer_draw_handler)
 start_button = frame.add_button('Start', start_btn_handler, 80)
 stopt_button = frame.add_button('Stop', stop_btn_handler, 80)
 reset_button = frame.add_button('Reset', reset_btn_handler, 80)
+
+# start frame
 
 frame.start()
 
