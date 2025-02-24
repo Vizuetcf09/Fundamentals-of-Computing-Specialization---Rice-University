23b7d000aa36f235be697788be3c04e10c351f8b
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2025-02-23 22:41:09 -0600

    modified: 3_stopwatch.py

--- courses/11/Mini-projects/3_stopwatch.py
+++ courses/11/Mini-projects/3_stopwatch.py
@@ -1,9 +1,14 @@
 # Stopwatch: The Game
 
 import simplegui
+import random
 
 # define global variables
 time = 1
+position = [60, 110]
+width = 200
+height = 200
+interval = 1800
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -23,11 +28,19 @@ def reset():
 
 # define event handler for timer with 0.1 sec interval
 def interval01():
-    pass
+    pass # agregar función para agregar un seundo al intervalo
 
 # define draw handler
 def timer_draw_handler(canvas):
-    canvas.draw_text(format(), [60, 110], 24, "White")
+    canvas.draw_text(format(), position, 24, "White")
+    
+# define handler timer
+def tick():
+    x = random.randrange(0, width)
+    y = random.randrange(0, height)
+    position[0] = x
+    position[1] = y
+
 
 # button handlers
 def start_btn_handler():
@@ -40,16 +53,18 @@ def reset_btn_handler():
     pass
 
 # create frame
-frame = simplegui.create_frame('Stopwatch: The Game', 200, 200)
+frame = simplegui.create_frame('Stopwatch: The Game', width, height)
 
-# start frame
+# hander register
 frame.set_draw_handler(timer_draw_handler)
 start_button = frame.add_button('Start', start_btn_handler, 80)
 stopt_button = frame.add_button('Stop', stop_btn_handler, 80)
 reset_button = frame.add_button('Reset', reset_btn_handler, 80)
+timer = simplegui.create_timer(interval, tick)
 
 # start frame
 
 frame.start()
+timer.start()
 
 # Please remember to review the grading rubric
