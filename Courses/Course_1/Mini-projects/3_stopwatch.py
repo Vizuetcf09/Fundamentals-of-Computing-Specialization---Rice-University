8e7e54bc8ae0b8e7ffa6b5fb763bef59d31a0186
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2025-03-10 20:12:48 -0600

    modified: stopwatch

--- Courses/Course_1/Mini-projects/3_stopwatch.py
+++ Courses/Course_1/Mini-projects/3_stopwatch.py
@@ -4,31 +4,48 @@ import simplegui
 import random
 
 # define global variables
-time = 1
+time = 0
 position = [60, 110]
 width = 200
 height = 200
-interval = 1800
+interval = 1000
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format():
     global time
-    return str(time)
+    global A
+    global B
+    global C
+    global D
+    global format_counter
+    
+    A = (time // 1000) % 10
+    B = (time // 100) % 10
+    C = (time // 10) % 10
+    D = time % 10
+    
+    if B >= 6:
+        B = 0
+        A += 1
+    
+    format_counter = str(A) + ":" + str(B) + str(C) + "." + str(round(D)) 
+    return format_counter
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
-    return 
+    timer.start()
 
 def stop():
-    pass
+    timer.stop()
 
 def reset():
-    pass
+    global time
+    time = 0
 
 # define event handler for timer with 0.1 sec interval
 def interval01():
-    pass # agregar función para agregar un seundo al intervalo
+    pass # agregar función para agregar un segundo al intervalo
 
 # define draw handler
 def timer_draw_handler(canvas):
@@ -36,21 +53,19 @@ def timer_draw_handler(canvas):
     
 # define handler timer
 def tick():
-    x = random.randrange(0, width)
-    y = random.randrange(0, height)
-    position[0] = x
-    position[1] = y
-
+    global time 
+    time += 1
+    return
 
 # button handlers
 def start_btn_handler():
-    pass
+    start()
 
 def stop_btn_handler():
-    pass
+    stop()
 
 def reset_btn_handler():
-    pass
+    reset()
 
 # create frame
 frame = simplegui.create_frame('Stopwatch: The Game', width, height)
@@ -65,6 +80,5 @@ timer = simplegui.create_timer(interval, tick)
 # start frame
 
 frame.start()
-timer.start()
 
 # Please remember to review the grading rubric
