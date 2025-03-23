b45041fcf5f59abe2b4420cebb6cac5b90975eb3
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2025-03-23 12:36:16 -0600

    stopwatch_final

--- Courses/Course_1/Mini-projects/3_stopwatch.py
+++ Courses/Course_1/Mini-projects/3_stopwatch.py
@@ -5,23 +5,17 @@ import random
 
 # define global variables
 time = 0
-#successful_stops = 0
-#total_stops = 0
+stops = 0
+true_stops = 0
 position = [60, 110]
-#score_position = [150, 30]
+score_position = [150, 30]
 width = 200
 height = 200
 interval = 100
 
-# define helper function format that converts time
-# in tenths of seconds into formatted string A:BC.D
-def format():
+def time_decomposition():
     global time
-    global A
-    global B
-    global C
-    global D
-    global format_counter
+    global A, B, C, D
     
     A = (time // 1000) % 10
     B = (time // 100) % 10
@@ -31,45 +25,49 @@ def format():
     if B >= 6:
         B = 0
         A += 1
+    else:
+        return
+    
+    return A, B, C, D
+
+# define helper function format that converts time
+# in tenths of seconds into formatted string A:BC.D
+
+def format():
+    time_decomposition()
     
     format_counter = str(A) + ":" + str(B) + str(C) + "." + str(round(D)) 
     return format_counter
-    
+
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
     timer.start()
 
 def stop():
-    timer.stop()
-
-#def stop():
-#    global total_stops, successful_stops
-#    if timer.is_running():
-#        timer.stop()
-#        total_stops += 1
-#        if time % 10 == 0:
-#            successful_stops += 1
-
+    global stops, true_stops
+    if timer.is_running():
+        timer.stop()
+        stops += 1
+        if time % 10 == 0:
+            true_stops += 1
+            
 def reset():
-    global time
+    global time, stops, true_stops
     time = 0
-
-#def reset():
-#    global time, successful_stops, total_stops
-#    time = 0
-#    successful_stops = 0
-#    total_stops = 0
+    stops = 0
+    true_stops = 0
 
 # define draw handler
+
 def timer_draw_handler(canvas):
     canvas.draw_text(format(), position, 24, "White")
-    #    canvas.draw_text(f"{successful_stops}/{total_stops}", score_position, 20, "Green")
+    canvas.draw_text("{}/{}".format(true_stops, stops), score_position, 24, "Red")
     
 # define handler timer
 def tick():
     global time 
     time += 1
-    return
+    return time
 
 # button handlers
 def start_btn_handler():
